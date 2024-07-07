661a1d07ea31715982372782b8d7316d872a599f
Gaurav Mahawar <thegauravmahawar@gmail.com>
2024-07-07 10:45:42 +0530

    Journal API

---
+++ journal/journal-api/src/main/java/api/JournalApiApplication.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApiApplication.class, args);
+	}
+
+}
