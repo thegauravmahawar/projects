1f3262987576df2defcac1df454f19e0a5f832c2
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-01-12 18:36:58 +0530

    Housing location component

---
+++ angular-house-listing-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+
+}
