8a508744f4a8ee99fe43423693c4b1b66c748a60
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-01-13 22:22:53 +0530

    Interpolation

--- angular-house-listing-app/src/app/housing-location/housing-location.component.ts
+++ angular-house-listing-app/src/app/housing-location/housing-location.component.ts
@@ -6,9 +6,16 @@ import { HousingLocation } from '../housing-location';
   selector: 'app-housing-location',
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.photo }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
