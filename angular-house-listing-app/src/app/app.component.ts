806845c50f2b5b4bde6a84e79339c2cd84565036
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-01-14 17:33:04 +0530

    Angular routing

--- angular-house-listing-app/src/app/app.component.ts
+++ angular-house-listing-app/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import {Component} from '@angular/core';
-import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
