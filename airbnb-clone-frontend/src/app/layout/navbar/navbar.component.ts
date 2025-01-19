497393b6a5433115a61919fa92a8c9430efc894d
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-01-19 22:33:24 +0530

    Airbnb Navbar

--- airbnb-clone-frontend/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {ToolbarModule} from 'primeng/toolbar';
+import {MenuModule} from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { CategoryComponent } from './category/category.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenuItem } from 'primeng/api';
+import { ToastService } from '../toast.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FontAwesomeModule, ToolbarModule, MenuModule, CategoryComponent, AvatarComponent],
+  providers: [DialogService],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  location = "Anywhere";
+  guests = "Add guests";
+  dates = "Any week";
+
+  toastService = inject(ToastService);
+  dialogService = inject(DialogService);
+  activatedRoute = inject(ActivatedRoute);
+  ref: DynamicDialogRef | undefined;
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(): void {
+    this.currentMenuItems = this.fetchMenu();
+  }
+
+  fetchMenu() {
+    return [
+      {
+        "label": "Sign up",
+        "styleClass": "font-bold"
+      },
+      {
+        "label": "Log in",
+      }
+    ];
+  }
 
 }
