e0417bf28e100e3760b87717c436414e24dd877d
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-01 13:26:12 +0530

    User model

--- airbnb-clone-backend/src/main/java/airbnbclonebackend/dao/User.java
+++ airbnb-clone-backend/src/main/java/airbnbclonebackend/dao/User.java
@@ -1,7 +1,13 @@
 package airbnbclonebackend.dao;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+@Data
+@EqualsAndHashCode
+@ToString
 @Entity
 @Table(name = "users")
 public class User extends AbstractAuditingEntity<Integer> {
@@ -10,6 +16,18 @@ public class User extends AbstractAuditingEntity<Integer> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "image_url")
+    private String imageUrl;
 
     @Override
     public Integer getId() {
