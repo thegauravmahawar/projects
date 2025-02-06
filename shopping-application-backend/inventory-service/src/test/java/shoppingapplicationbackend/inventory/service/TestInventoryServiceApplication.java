1cc315a9bddfcbb3b2ee2a005b303071d9f4d23a
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-06 18:55:39 +0530

    Inventoory service

---
+++ shopping-application-backend/inventory-service/src/test/java/shoppingapplicationbackend/inventory/service/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package shoppingapplicationbackend.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
