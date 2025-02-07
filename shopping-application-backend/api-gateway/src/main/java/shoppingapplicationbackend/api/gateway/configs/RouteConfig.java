5c847b9043cccc308294c179e0897d93501976e0
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-07 10:28:53 +0530

    Add rouutes for Order and Inventory service

--- shopping-application-backend/api-gateway/src/main/java/shoppingapplicationbackend/api/gateway/configs/RouteConfig.java
+++ shopping-application-backend/api-gateway/src/main/java/shoppingapplicationbackend/api/gateway/configs/RouteConfig.java
@@ -19,4 +19,22 @@ public class RouteConfig {
                         HandlerFunctions.http("http://localhost:8080")
                 ).build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(
+                        RequestPredicates.path("/api/orders"),
+                        HandlerFunctions.http("http://localhost:8081")
+                ).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(
+                        RequestPredicates.path("/api/inventory"),
+                        HandlerFunctions.http("http://localhost:8082")
+                ).build();
+    }
 }
