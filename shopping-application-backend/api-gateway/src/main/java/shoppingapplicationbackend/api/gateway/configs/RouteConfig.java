890077e0053db9f3dd279bd5561d46292b2d56fd
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-07 10:22:53 +0530

    Setup product route

---
+++ shopping-application-backend/api-gateway/src/main/java/shoppingapplicationbackend/api/gateway/configs/RouteConfig.java
@@ -0,0 +1,22 @@
+package shoppingapplicationbackend.api.gateway.configs;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service")
+                .route(
+                        RequestPredicates.path("/api/products"),
+                        HandlerFunctions.http("http://localhost:8080")
+                ).build();
+    }
+}
