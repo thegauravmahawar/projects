cc2049f2577ac5d4d31f37a355c21029b6ff75fa
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-10 21:46:09 +0530

    Setup Swagger

---
+++ shopping-application-backend/product-service/src/main/java/shoppingapplicationbackend/product/service/configs/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package shoppingapplicationbackend.product.service.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.SpecVersion;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product API Documentation")
+                        .version("v0.0.1")
+                );
+    }
+}
