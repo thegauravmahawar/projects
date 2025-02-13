753e075d758dcd6a2a07d2a251516218a7b7cbcd
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-13 22:43:29 +0530

    Setup rrequest factory

--- shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/clients/InventoryClient.java
+++ shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/clients/InventoryClient.java
@@ -1,10 +1,18 @@
 package shoppingapplicationbackend.order.service.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        return false;
+    }
 }
--- shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/configs/RestClientConfig.java
+++ shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/configs/RestClientConfig.java
@@ -1,12 +1,17 @@
 package shoppingapplicationbackend.order.service.configs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import shoppingapplicationbackend.order.service.clients.InventoryClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,9 +23,17 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(INVENTORY_SERVICE_URL)
+                .requestFactory(getRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    public HttpComponentsClientHttpRequestFactory getRequestFactory() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectionRequestTimeout(Duration.ofSeconds(3));
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+        return requestFactory;
     }
 }
