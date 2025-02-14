d4c3a3ce091baac3683bd9bd31718c81ccf09708
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-14 20:05:48 +0530

    Todos ccomponents

---
+++ angular-todos-app/src/app/todos/todos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos.component.html'
+})
+export class TodosComponent {
+  title = 'todos';
+}
