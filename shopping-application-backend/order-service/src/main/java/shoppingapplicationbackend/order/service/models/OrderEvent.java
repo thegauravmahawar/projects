267b38f76f86684e3c2ce1e6294639b1b74cf751
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-23 00:30:13 +0530

    Kafka ssetup

---
+++ shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/models/OrderEvent.java
@@ -0,0 +1,13 @@
+package shoppingapplicationbackend.order.service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String orderNumber;
+}
--- shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/services/OrderService.java
+++ shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/services/OrderService.java
@@ -1,10 +1,12 @@
 package shoppingapplicationbackend.order.service.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shoppingapplicationbackend.order.service.clients.InventoryClient;
 import shoppingapplicationbackend.order.service.dao.Order;
+import shoppingapplicationbackend.order.service.models.OrderEvent;
 import shoppingapplicationbackend.order.service.repositories.OrderRepository;
 import shoppingapplicationbackend.order.service.resources.OrderResource;
 
@@ -16,6 +18,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate kafkaTemplate;
 
     @Transactional(rollbackFor = Throwable.class)
     public Order placeOrder(OrderResource orderResource) {
@@ -29,6 +32,11 @@ public class OrderService {
         }
 
         order.setOrderNumber(UUID.randomUUID().toString());
-        return orderRepository.save(order);
+        order = orderRepository.save(order);
+
+        OrderEvent orderEvent = new OrderEvent(order.getOrderNumber());
+        kafkaTemplate.send("order-topic", orderEvent);
+
+        return order;
     }
 }
