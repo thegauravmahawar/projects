c4f2dc89dc1e07dbf6013510208c76183598675e
Gaurav Mahawar <thegauravmahawar@gmail.com>
2025-02-24 23:23:33 +0530

    Fix RRestClientConfig

--- shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/configs/RestClientConfig.java
+++ shopping-application-backend/order-service/src/main/java/shoppingapplicationbackend/order/service/configs/RestClientConfig.java
@@ -1,11 +1,9 @@
 package shoppingapplicationbackend.order.service.configs;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -21,19 +19,22 @@ public class RestClientConfig {
 
     @Bean
     public InventoryClient inventoryClient() {
+        // Create a simple request factory with timeouts
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        requestFactory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(INVENTORY_SERVICE_URL)
-                .requestFactory(getRequestFactory())
+                .requestFactory(requestFactory)
                 .build();
-        var restClientAdapter = RestClientAdapter.create(restClient);
-        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
-        return httpServiceProxyFactory.createClient(InventoryClient.class);
-    }
 
-    public HttpComponentsClientHttpRequestFactory getRequestFactory() {
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setConnectionRequestTimeout(Duration.ofSeconds(3));
-        requestFactory.setReadTimeout(Duration.ofSeconds(3));
-        return requestFactory;
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+
+        // Use the builderFor method which is available in most Spring Boot versions
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter)
+                .build();
+
+        return factory.createClient(InventoryClient.class);
     }
 }
